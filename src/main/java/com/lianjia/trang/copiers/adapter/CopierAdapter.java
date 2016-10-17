8896dfcdddf48aebffddc4e5ecdb76f3fa19d856
donghaoliang <donghaoliang@lianjia.com>
2016-10-17 12:18:07 +0800

    添加适配器

---
+++ src/main/java/com/lianjia/trang/copiers/adapter/CopierAdapter.java
@@ -0,0 +1,51 @@
+package com.lianjia.trang.copiers.adapter;
+
+import com.lianjia.trang.copiers.inter.Copier;
+
+/**
+ * Copier的适配器，可继承该类实现具体的拷贝过程，也可直接实现#{@link Copier}接口
+ * 
+ * @author trang
+ */
+public abstract class CopierAdapter<C, F, T> implements Copier<F, T> {
+	// 实际执行拷贝的类
+	private C copier;
+	// 源类
+	private Class<F> sourceClass;
+	// 目标类
+	private Class<T> targetClass;
+	// 反转Copier，lazyinit，在调用时才初始化
+	private C reverse;
+	
+	/**
+	 * 反转Copier，子类需定义反转的过程
+	 * 
+	 * @return
+	 */
+	public abstract C reverse();
+	
+	/**
+	 * 子类可以访问的构造方法
+	 * 
+	 * @param sourceClass
+	 * @param targetClass
+	 */
+	protected CopierAdapter(Class<F> sourceClass, Class<T> targetClass, C copier) {
+		this.sourceClass = sourceClass;
+		this.targetClass = targetClass;
+		this.copier = copier;
+	}
+	
+	public C getCopier() {
+		return copier;
+	}
+	public Class<F> getSourceClass() {
+		return sourceClass;
+	}
+	public Class<T> getTargetClass() {
+		return targetClass;
+	}
+	public C getReverse() {
+		return reverse;
+	};
+}
